686a7e4f2124279710550ec05a3d9a98677ee0c2
Yaroslav Melnichuk <nemish.i.nelos@gmail.com>
2015-06-22 12:50:50 +0300

    fx

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,21 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var less = require('gulp-less');
 
-gulp.task('default', function () {
-    gutil.log('ololo');
+var paths = {
+    less: {
+        src: 'server/static/less/**/*.less',
+        dest: 'server/static/css'
+    }
+};
+
+gulp.task('less', function () {
+  return gulp.src(paths.less.src)
+    .pipe(less())
+    .pipe(gulp.dest(paths.less.dest));
 });
+
+gulp.task('watch', function() {
+  gulp.watch(paths.less.src, ['less']);
+});
+
