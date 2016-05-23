c98b931019e4fd3068fc37e358ca6d4f57ca4fe4
Yaroslav Melnichuk <nemish.i.nelos@gmail.com>
2016-05-23 15:01:46 +0300

    fx

--- server/app/scripts/send_emails.py
+++ server/app/scripts/send_emails.py
@@ -3,7 +3,7 @@ from app.models import Message
 
 
 def send():
-    msgs = Message.query.filter(Message.send_on=None)
+    msgs = Message.query.filter(Message.send_on==None)
     for msg in msgs:
         print 'sending msg', msg.id
         msg.notify()
